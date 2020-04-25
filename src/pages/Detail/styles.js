4ce9e449bc1677605b4afb31b006a0de3aecef4d
Renato Bezerra <rbezerra@sealtelecom.com.br>
2020-04-25 03:05:28 -0300

    Added detais page with send mail and whatsapp

---
+++ src/pages/Detail/styles.js
@@ -0,0 +1,89 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+    backgroundColor: "#EDEDED",
+  },
+
+  title: {
+    marginTop: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 30
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+
+  incident:{
+    padding: 24,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginBottom: 16,
+    marginTop: 26
+  },
+
+  incidentProperty:{
+    fontSize: 14,
+    fontWeight: "bold",
+    color: '#41414d',
+    marginTop: 24,
+  },
+
+  incidentValue:{
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380',
+  },
+
+
+  contactBox: {
+    backgroundColor: "#FFF",
+    padding: 24,
+    borderRadius: 8,
+    marginBottom: 16
+  },
+
+  heroTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
+
+  heroDescription:{
+    fontSize: 15,
+    color: '#737380',
+    marginTop: 16,
+  },
+
+  actions: {
+    marginTop: 16,
+    flexDirection: "row",
+    justifyContent: 'space-between'
+  },
+
+  action: {
+    backgroundColor: "#E02041",
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  actionText: {
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: "bold",
+  }
+
+});
